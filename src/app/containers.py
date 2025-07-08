6c4e1ba5a9854acacfefe1ed1dee55d817f8d8e1
Garis888 <garis8@yandex.ru>
2025-07-08 15:42:05 +0300

    Delete src directory

--- src/app/containers.py
+++
@@ -1,7 +0,0 @@
-from src.modules.service1.containers import containers_dict as monitoring_containers
-from src.core.containers import ContainerBuilder
-
-all_containers = []
-all_containers.extend(monitoring_containers)
-
-container = ContainerBuilder().build(all_containers)
--- src/modules/service1/router/__init__.py
+++
@@ -1,5 +0,0 @@
-from .service1_v1_router import router as service1_v1_router
-
-routers = [
-    service1_v1_router,
-]
